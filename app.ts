c1698032b14d516decb76ba02df8a8a4d523ca14
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-04-23 15:31:35 -0300

    Criando o meu próprio Types

--- app.ts
+++ app.ts
@@ -6,8 +6,6 @@ nome = "Stella Sophia Campelo";
 sobrenome = "Rodrigues"
 idade = 28;
 
-
-
 function somar(a: number, b:number){
     return a + b;
 }
@@ -15,3 +13,39 @@ console.log(somar(5, 10));
 
 
 console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
+
+let a: (number | string)[] = [];
+a.push(1);
+a.push(2);
+a.push(3);
+a.push(4);
+a.push(5);
+a.push(6);
+a.push("Stella Sophia Campelo Rodrigues")
+a.push("Laura Vitória Campelo Rodrigues")
+a.push("Luis Felipe Rodrigues Rodrigues")
+
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+
+type Animal = {
+    nome: string;
+    idade?: number;
+}
+
+let cachorro: Animal;
+
+cachorro = {
+    nome: "RuffleS",
+    idade: 28
+}
+console.log(cachorro)
--- dist/app.js
+++ dist/app.js
@@ -10,3 +10,30 @@ function somar(a, b) {
 }
 console.log(somar(5, 10));
 console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
+let a = [];
+a.push(1);
+a.push(2);
+a.push(3);
+a.push(4);
+a.push(5);
+a.push(6);
+a.push("Stella Sophia Campelo Rodrigues");
+a.push("Laura Vitória Campelo Rodrigues");
+a.push("Luis Felipe Rodrigues Rodrigues");
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+a.pop();
+console.log(a);
+let cachorro;
+cachorro = {
+    nome: "RuffleS",
+    idade: 28
+};
+console.log(cachorro);
