2819ce01eb95b2aceb86aeeec2ad25ec22dc8057
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-04-23 17:01:23 -0300

    ENUMS

--- app.ts
+++ app.ts
@@ -1,51 +1,25 @@
-let nome: string;
-let sobrenome: string;
-let idade: number;
-
-nome = "Stella Sophia Campelo";
-sobrenome = "Rodrigues"
-idade = 28;
-
-function somar(a: number, b:number){
-    return a + b;
-}
-console.log(somar(5, 10));
-
-
-console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
-
-let a: (number | string)[] = [];
-a.push(1);
-a.push(2);
-a.push(3);
-a.push(4);
-a.push(5);
-a.push(6);
-a.push("Stella Sophia Campelo Rodrigues")
-a.push("Laura Vitória Campelo Rodrigues")
-a.push("Luis Felipe Rodrigues Rodrigues")
-
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-
-type Animal = {
-    nome: string;
-    idade?: number;
+enum ProdutoStatus{
+    Ativo = 1,
+    Inativo = 2,
+    Indisponivel = 3
 }
 
-let cachorro: Animal;
 
-cachorro = {
-    nome: "RuffleS",
-    idade: 28
+function checarProduto(status: number){
+
+    switch(status){
+        case ProdutoStatus.Ativo:
+            console.log("Produto Disponível")
+            break;
+        
+        case ProdutoStatus.Inativo:
+            console.log("Produto Não Encontrado");
+            break;
+
+        case ProdutoStatus.Indisponivel:
+            console.log("Produto Aguardando Estoque")
+            break;
+    }
 }
-console.log(cachorro)
+
+checarProduto(1);
--- dist/app.js
+++ dist/app.js
@@ -1,39 +1,21 @@
 "use strict";
-let nome;
-let sobrenome;
-let idade;
-nome = "Stella Sophia Campelo";
-sobrenome = "Rodrigues";
-idade = 28;
-function somar(a, b) {
-    return a + b;
+var ProdutoStatus;
+(function (ProdutoStatus) {
+    ProdutoStatus[ProdutoStatus["Ativo"] = 1] = "Ativo";
+    ProdutoStatus[ProdutoStatus["Inativo"] = 2] = "Inativo";
+    ProdutoStatus[ProdutoStatus["Indisponivel"] = 3] = "Indisponivel";
+})(ProdutoStatus || (ProdutoStatus = {}));
+function checarProduto(status) {
+    switch (status) {
+        case ProdutoStatus.Ativo:
+            console.log("Produto Disponível");
+            break;
+        case ProdutoStatus.Inativo:
+            console.log("Produto Não Encontrado");
+            break;
+        case ProdutoStatus.Indisponivel:
+            console.log("Produto Aguardando Estoque");
+            break;
+    }
 }
-console.log(somar(5, 10));
-console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
-let a = [];
-a.push(1);
-a.push(2);
-a.push(3);
-a.push(4);
-a.push(5);
-a.push(6);
-a.push("Stella Sophia Campelo Rodrigues");
-a.push("Laura Vitória Campelo Rodrigues");
-a.push("Luis Felipe Rodrigues Rodrigues");
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-a.pop();
-console.log(a);
-let cachorro;
-cachorro = {
-    nome: "RuffleS",
-    idade: 28
-};
-console.log(cachorro);
+checarProduto(1);
