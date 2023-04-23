6a15e415e5dcb78e79c465090047e3ff580de083
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-04-22 22:54:31 -0300

    Executando Typescript e o npx ts-node-dev

---
+++ app.ts
@@ -0,0 +1,10 @@
+let nome: string;
+let sobrenome: string;
+let idade: number;
+
+nome = "Luis Felipe";
+sobrenome = "Rodrigues"
+idade = 28;
+
+
+console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let nome;
+let sobrenome;
+let idade;
+nome = "RuffleS";
+sobrenome = "Rodrigues";
+idade = 28;
+console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
